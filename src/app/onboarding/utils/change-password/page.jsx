879d83a6226eeba81c3419227864b503b5e27b23
Edgar Jason Husin <edgarjasonhusin@gmail.com>
2026-05-21 15:10:49 +0700

    feat: add change password page to onboarding flow

--- src/app/onboarding/utils/change-password/page.jsx
+++ src/app/onboarding/utils/change-password/page.jsx
@@ -2,31 +2,31 @@ import OnboardingTemplate from "@/components/templates/OnboardingTemplate";
 import { Input } from "@/components/ui/input";
 import { Field, FieldLabel, FieldContent } from "@/components/ui/field";
 
-export default function ChangePassword(){
-    return(
+export default function ChangePassword() {
+    return (
         <main>
             <OnboardingTemplate
-            title="Ubah Password"
-            caption="Masukkan password baru dan konfirmasi password baru Anda"
-            children={
-                <div className="grid grid-cols gap-6 w-full text-left">
-                    <Field orientation="vertical">
-                        <FieldLabel htmlFor="passwordBaru">Masukkan Password Baru</FieldLabel>
-                        <FieldContent>
-                            <Input type="password" id="passwordBaru" placeholder="Masukkan password baru Anda" required />
-                        </FieldContent>
-                    </Field>
+                title="Ubah Password"
+                caption="Masukkan password baru dan konfirmasi password baru Anda"
+                children={
+                    <div className="grid grid-cols gap-6 w-full text-left">
+                        <Field orientation="vertical">
+                            <FieldLabel htmlFor="passwordBaru">Masukkan Password Baru</FieldLabel>
+                            <FieldContent>
+                                <Input type="password" id="passwordBaru" placeholder="Masukkan password baru Anda" required />
+                            </FieldContent>
+                        </Field>
 
-                    <Field orientation="vertical">
-                        <FieldLabel htmlFor="konfirmasiPasswordBaru">Konfirmasi Password Baru</FieldLabel>
-                        <FieldContent>
-                            <Input type="password" id="konfirmasiPasswordBaru" placeholder="Masukkan password baru Anda" required />
-                        </FieldContent>
-                    </Field>
-                </div>
-            }
-            button="Ubah Password"
-            disabled={false}
+                        <Field orientation="vertical">
+                            <FieldLabel htmlFor="konfirmasiPasswordBaru">Konfirmasi Password Baru</FieldLabel>
+                            <FieldContent>
+                                <Input type="password" id="konfirmasiPasswordBaru" placeholder="Masukkan password baru Anda" required />
+                            </FieldContent>
+                        </Field>
+                    </div>
+                }
+                button="Ubah Password"
+                disabled={false}
             >
             </OnboardingTemplate>
         </main>
